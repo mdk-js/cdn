4afd9c9314c227b8867638da39176aafc735bc8d
hans000 <hhance@163.com>
2022-11-21 22:11:17 +0800

    feat: update recipe json

--- packages/m/mc-recipes/src/index.js
+++ packages/m/mc-recipes/src/index.js
@@ -4,6 +4,16 @@ const path = require('path')
 const versions = ['1.13', '1.14', '1.15', '1.16', '1.17', '1.18', '1.19']
 
 versions.forEach(v => {
-    const list = fs.readdirSync(path.resolve(__dirname, '../dist', v)).map(e => e.slice(0, -5))
+    const dir = path.resolve(__dirname, '../dist/recipes', v)
+    const list = fs.readdirSync(dir).map(e => {
+        const result = JSON.parse(fs.readFileSync(path.resolve(dir, e))).result
+        if (result === undefined) {
+            return undefined
+        }
+        return {
+            file: e.slice(0, -5),
+            id: typeof result === 'string' ? result.slice(10) : result.item.slice(10)
+        }
+    }).filter(Boolean)
     fs.writeFileSync(path.resolve(__dirname, '../dist', v + '.json'), JSON.stringify(list, null, 4))
 })
